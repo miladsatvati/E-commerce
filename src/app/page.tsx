c71da7434c72cdaffad61bf809c5272809184435
milad satvati <miladsatvati@gmail.com>
2024-10-16 13:32:38 +0330

    start DropZone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Icons } from "@/components/Icons";
 import MaxWithWrapper from "@/components/MaxWithWrapper";
 import Phone from "@/components/Phone";
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check, Star } from "lucide-react";
 import Reviews from "@/components/Reviews";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -162,11 +164,14 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  "I usually keep my phone together with my keys in my pocket and that led to some pretty heavy scratchmarks on all of my last phone caases. this one, besides a barelt noticeable scratch on the corner, {' '}
+                  "I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratchmarks on all of my
+                  last phone caases. this one, besides a barelt noticeable
+                  scratch on the corner,{" "}
                   <span className="p-0.5 bg-purple-950 text-white traking m-px">
                     looks brand new after about half a year
                   </span>{" "}
-                   . I dig it"
+                  . I dig it"
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -190,6 +195,61 @@ export default function Home() {
           <Reviews />
         </div>
       </section>
+      <section>
+        <MaxWithWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get{" "}
+                <span className="px-2 bg-purple-600 text-white">
+                  you own case
+                </span>{" "}
+                say
+              </h2>
+            </div>
+          </div>
+          <div className="mx-auto maxw-6xl lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40 ">
+              <img
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+              />
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img
+                  src="/horse.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-600 inline mr-1.5" />
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-600 inline mr-1.5" />
+              Scratch and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-600 inline mr-1.5" />5 year
+              print warranty
+            </li>
+            <div className="flex justify-center">
+              <Link className={buttonVariants({
+                size: 'lg',
+                className: "mx-auto mt-8"
+              })} href='/configure/upload'>create your case now <ArrowRight className="h-4 w-4 ml-1.5" /></Link>
+            </div>
+          </ul>
+        </MaxWithWrapper>
+      </section>
     </div>
   );
 }
+ 
